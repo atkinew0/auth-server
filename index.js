0c5fd67a4543b34854e294a6842eb7f3bda75fce
Eli Atkinson <atkinson.eli@gmail.com>
2019-01-17 18:05:44 -0500

    finalized server with cors

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 mongoose.connect('mongodb://localhost:3091');
 
 //App Setuup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({type:'*/*'}));
 router(app);
 
@@ -18,6 +20,7 @@ router(app);
 
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
+
 
 server.listen(port, () => {
     console.log("Server listening on port", port);
--- router.js
+++ router.js
@@ -6,9 +6,14 @@ const requireAuth = passport.authenticate('jwt', {session: false});
 const requireSignin = passport.authenticate('local', {session: false});
 
 module.exports = function(app){
-    app.get('/', requireAuth, function(req,res){
-        res.send("Hi There");
+    
+    app.get('/', function(req,res){
+        console.log("Hit basic");
+        res.send("Hi you")
     })
+    // app.get('/', requireAuth, function(req,res){
+    //     res.send("Hi There");
+    // })
 
     app.post('/signin', requireSignin, authentication.signin);
     app.post('/signup', authentication.signup);
